c950d3340b803808c0b885f97fc94c08c7a00383
Hardikkhanesa <hkhanesa@gmail.com>
2020-07-17 11:43:22 +0530

    contact page

--- src/portfolio.js
+++ src/portfolio.js
@@ -512,7 +512,7 @@ const projectsHeader = {
 const contactPageData = {
 	contactSection: {
 		title: "Contact Me",
-		profile_image_path: "animated_ashutosh.png",
+		profile_image_path: "hkimage.jpg",
 		description:
 			"I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
 	},
@@ -520,19 +520,20 @@ const contactPageData = {
 		title: "Blogs",
 		subtitle:
 			"For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-		link: "https://ashutoshhathidara.wordpress.com",
+		link: "https://hardikkhanesa.blogspot.com/",
 		avatar_image_path: "blogs_image.svg",
 	},
 	addressSection: {
 		title: "Address",
 		subtitle:
-			"Ambavadi vas, Kanodar, T.A.-Palanpur, Dist.-Banaskantha, Gujarat - 385520",
+			"Nikol,Ahmedabad,Gujarat",
 		avatar_image_path: "address_image.svg",
-		location_map_link: "https://goo.gl/maps/MpMqtRZytFchMkZ76",
+		location_map_link: "https://goo.gl/maps/zr1YvEN57Z2gtpW56",
 	},
+
 	phoneSection: {
 		title: "Phone Number",
-		subtitle: "+91 8320758513",
+		subtitle: "+91 *********",
 	},
 };
 
