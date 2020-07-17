8a15c9b90d5943eda068781c33361122b0da9c8e
Hardikkhanesa <hkhanesa@gmail.com>
2020-07-17 11:53:05 +0530

    projects updated

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -45,7 +45,7 @@ class Projects extends Component {
 						<Button
 							text={"More Projects"}
 							className="project-button"
-							href="https://github.com/ashutosh1919"
+							href="https://github.com/Hardikkhanesa"
 							newTab={true}
 							theme={theme}
 						/>
